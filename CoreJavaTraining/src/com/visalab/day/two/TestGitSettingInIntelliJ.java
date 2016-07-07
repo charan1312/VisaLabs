a41c1eb48cbffdf64cc2f0831c1c7d55b4839e91
Charan Thej Aware <charan.1312@gmail.com>
2016-07-07 16:30:13 -0500

    Adding the day1, day2 and day3 weather labs into a single repository

---
+++ CoreJavaTraining/src/com/visalab/day/two/TestGitSettingInIntelliJ.java
@@ -0,0 +1,7 @@
+package com.visalab.day.two;
+
+/**
+ * Created by CAWARE on 7/6/2016.
+ */
+public class TestGitSettingInIntelliJ {
+}
---
+++ TemperatureConversion/src/com/pluralsight/getorganized/TempConverter.java
@@ -0,0 +1,16 @@
+package com.pluralsight.getorganized;
+import java.util.Scanner;
+
+public class TempConverter {
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature in Fahrenheit:");
+        double f = Double.parseDouble(sc.nextLine());
+        double c = 5*(f-32)/9;
+        System.out.println("Temperature in celsius:" +c);
+        sc.close();
+
+    }
+}
