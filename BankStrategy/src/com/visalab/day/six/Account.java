152bd89fc03b06d2653e467891b1a6df87f19927
Charan Thej Aware <charan.1312@gmail.com>
2016-07-15 18:25:17 -0500

    Changing the Main method from Account class to Main class

--- BankStrategy/src/com/visalab/day/six/Account.java
+++ BankStrategy/src/com/visalab/day/six/Account.java
@@ -108,14 +108,5 @@ public class Account {
         }
     }
 
-    public static void main(String[] args) {
-        Account charan = new Account("Charan",500,"Normal");
-        charan.deposit(500);
-        charan.withdraw(200);
-        charan.withdraw(300);
-        charan.withdraw(300);
-        charan.withdraw(200);
-        charan.setAcntType("Special");
-        charan.withdraw(200);
-    }
+
 }
--- BankStrategy/src/com/visalab/day/six/Main.java
+++ BankStrategy/src/com/visalab/day/six/Main.java
@@ -3,6 +3,13 @@ package com.visalab.day.six;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Account charan = new Account("Charan",500,"Normal");
+        charan.deposit(500);
+        charan.withdraw(200);
+        charan.withdraw(300);
+        charan.withdraw(300);
+        charan.withdraw(200);
+        charan.setAcntType("Special");
+        charan.withdraw(200);
     }
 }
